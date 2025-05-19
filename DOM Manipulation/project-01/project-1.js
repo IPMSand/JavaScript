29c36b16da8de2ab04f62320952da5f50ac3bf45
IPMSand <131543563+IPMSand@users.noreply.github.com>
2025-05-19 21:09:48 +0530

    project 2 created

--- DOM Manipulation/project-01/project-1.js
+++ DOM Manipulation/project-01/project-1.js
@@ -1,7 +1,7 @@
 // variables
 
 let btn = document.querySelector('#new-quo');
-let quo = document.querySelector('.quo');
+let quoe = document.querySelector('.quo');
 let person = document.querySelector('.person');
 
 // quotes array
@@ -26,11 +26,11 @@ const quolist = [{
     quo: '“The fool doth think he is wise, but the wise man knows himself to be a fool.” ',
     person: '― William Shakespeare, As You Like It '
 },
-]
+];
 
 btn.addEventListener('click', function(){
     let random = Math.floor(Math.random()*quolist.length);
 
-    quo.innerHTML = quolist[random].quo;
-    person.innerHTML = quolist[random].quo;
+    quoe.innerHTML = quolist[random].quo;
+    person.innerHTML = quolist[random].person;
 })
